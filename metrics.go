68d4c69a05439085ee8e87445d1f53d38e8ef1e4
IliaWindowsMachine <iliaar@qognify.com>
2020-11-02 12:17:29 +0200

    package fix

--- metrics.go
+++ metrics.go
@@ -1,4 +1,4 @@
-package main
+package prometheusconsul
 
 type MetricResponse struct {
 	Gauges []Gauge `json:"Gauges"`
--- scraper.go
+++ scraper.go
@@ -1,4 +1,4 @@
-package main
+package prometheusconsul
 
 import (
 	"encoding/json"
